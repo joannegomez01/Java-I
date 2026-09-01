a31d3ec62145f27560982e741daf94d7c09878cf
Joanne Gomez <112110996+joannegomez01@users.noreply.github.com>
2026-09-01 03:44:17 -0700

    Add files via upload
    W1: Getting started w/ Java

---
+++ HelloWorld.java
@@ -0,0 +1,35 @@
+/*
+
+NAME: Joanne Gomez
+COURSE: CSIS24 (JAVA I)
+DATE: 01/29/26
+PROGRAM NAME: HelloWorld.java
+DESCRIPTION: Sample program typed up
+*/
+
+// any line that starts with // is a comment
+// comment are "explanations" , not considered as "code" by java
+
+// ON TOP OF EVERY PROGRAM YOU NEED THE FOLLOWING COMMENTS:
+// Name: Joanne Gomez
+// Date: 1/29/26
+// Description: Show "Hello World" in the command line/terminal
+// File Name: HelloWorld.java
+// To complie in terminal type: javac HelloWorld.java
+// To run the program in terminal type: java HelloWorld
+
+class HelloWorld
+{	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	} // end of main() method
+}// end of class HelloWorld
+
+/* Multiline comment that shows how program ran:
+
+PS C:\Users\joann\Desktop\CSIS24-JAVA I\W1 Getting Started With Java> javac HelloWorld.java
+PS C:\Users\joann\Desktop\CSIS24-JAVA I\W1 Getting Started With Java> java HelloWorld
+Hello World
+PS C:\Users\joann\Desktop\CSIS24-JAVA I\W1 Getting Started With Java>
+ */
---
+++ ShowNewLines.java
@@ -0,0 +1,29 @@
+/*
+
+NAME: Joanne Gomez
+COURSE: CSIS24 (JAVA I)
+DATE: 01/29/26
+PROGRAM NAME: HelloWorld.java
+DESCRIPTION: Sample program typed up
+*/
+
+class ShowNewLines
+{
+	public static void main(String[] args)
+	{
+		System.out.print("One \n two three \n"); // \n shows a new line
+		System.out.print("four five six");
+		System.out.println("...seven eight");
+		System.out.print("nine");
+	} // end of main() method
+} // end of class ShowNewLines
+
+/*
+PS C:\Users\joann\Desktop\CSIS24-JAVA I\W1 Getting Started With Java> javac ShowNewLines.java
+PS C:\Users\joann\Desktop\CSIS24-JAVA I\W1 Getting Started With Java> java ShowNewLines
+One
+ two three
+four five six...seven eight
+nine
+PS C:\Users\joann\Desktop\CSIS24-JAVA I\W1 Getting Started With Java>
+*/
